2094eeb0457087a8a1f9f882dc9ea390ea5fd0cb
Koriya Yukimai <yukimaikoriya@vip.qq.com>
2021-10-26 12:05:53 +0800

    Update chrome options

--- entrypoint.py
+++ entrypoint.py
@@ -35,6 +35,8 @@ if __name__ == '__main__':
     print('Driver Launching...')
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
     assert os.path.isfile(sys_path())
     driver_pjs = webdriver.Chrome(
         options=chrome_options,
